48e81a189dff86eb69d5425c389cf14fb53c95d6
sreebulusu <sbulusu@uh.edu>
2018-12-14 15:23:03 -0600

    new

---
+++ datapoint.py
@@ -0,0 +1,6 @@
+class datapoint:
+    def __init__(self, time, anchor, power, distance):
+        self.time = time
+        self.anchor = anchor
+        self.power = power
+        self.distance = distance
