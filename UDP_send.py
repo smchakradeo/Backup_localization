b2a200a8f1c15777cc438ab07e8494eb7f4ae7e7
smchakradeo <34455332+smchakradeo@users.noreply.github.com>
2018-12-14 11:52:43 -0600

    Create UDP_send.py
    send data from raspberry pi to the computer using UDP
    Use ip address of Computer

---
+++ UDP_send.py
@@ -0,0 +1,13 @@
+import socket
+
+s = socket.socket()
+host = '192.168.50.254' #ip of raspberry pi
+port = 12345
+s.bind((host, port))
+
+s.listen(5)
+while True:
+  c, addr = s.accept()
+  print ('Got connection from',addr)
+  c.send('Hello There!')
+  c.close()
