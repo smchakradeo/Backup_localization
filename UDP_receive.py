dd4b1b0f5b6199fc2d39b62bad0e0c291f7b5402
smchakra <kshubhamchakradeo@gmail.com>
2018-12-26 14:50:42 -0600

    Default Changelist

--- UDP_receive.py
+++ UDP_receive.py
@@ -1,8 +1,8 @@
 import socket
-
-s = socket.socket()
-host = '192.168.50.231'# ip of raspberry pi
-port = 12345
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
+while 1:
+    s = socket.socket()
+    host = '192.168.50.231'# ip of raspberry pi
+    port = 12345
+    s.connect((host, port))
+    print(s.recv(1024))
+    s.close()
