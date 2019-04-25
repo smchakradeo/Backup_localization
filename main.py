c029110b3f531c2c616978ba8bb186c0a450d601
smchakra <kshubhamchakradeo@gmail.com>
2019-04-25 12:12:27 -0500

    Default Changelist

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import socket
-import random
-import json
 from datapoint import datapoint
-import time
 import reset_current_datapoints as rcd
 import trilat as tri
 import numpy as np
+import matplotlib.pyplot as plt
 class Anchor:
     def __init__(self, id, x, y, z):
         self.id = id
@@ -13,6 +10,10 @@ class Anchor:
         self.y = y
         self.z = z
 
+
+
+
+"""
 class Tag:
     def __init__(self, time_T, ip, user):
         self.ip = ip
@@ -71,14 +72,9 @@ id, x, y, z = [], [], [], []
 ip, user = [], []
 thresh = 2
 
-socket_for_sending = socket.socket()
-host = '192.168.50.145'
-port = 12345
-socket_for_sending.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_for_sending.bind((host,port))
-socket_for_sending.listen(1) 
 
-tag = Tag(time.time(),host,"vehicle1")
+
+tag = Tag(time.time(),"vehicle1")
 for i in anchors:
     for j in range(len(i)):
         id.append(i[j]["id"])
@@ -92,41 +88,47 @@ for i in tags:
         ip.append(i[j]["ip"])
         user.append(i[j]["user"])
 #create UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('127.0.0.1',5678)
-while 1:
-    #On board sensor collection
-    rdm_num = random.randint(1,100)
-    message2 = str(rdm_num) + ' 51'
-    sent2 = sock.sendto(bytes(message2.encode()), server_address)
-    data2, server2 = sock.recvfrom(4096)
-    data2 = data2.decode("utf-8")
-    data2 = data2.strip('{ }')
-    data2 = data2.split()
-    for i in id:
-        message1 = str(rdm_num) + ' 50' + ' '+ i
-        try:
+"""
 
-            # Send data
-            sent1 = sock.sendto(bytes(message1.encode()), server_address)
-            # Receive response
-            data1, server1 = sock.recvfrom(4096)
-            data1 = data1.decode("utf-8")
-            data1 = data1.strip('{ }')
-            data1 = data1.split()
-            print('data1: ', data1) 
-            if((int(data1[1]) == 1) and (not (int(data2[1]) == 255)) and len(data2) == 14):
-                x = datapoint(time.time(),data1[3],data1[4],data1[2])
-                values = x.get_dp()
-                dplist[data1[3]] = values
-                currentDataPoints = rcd.resetcurrentdatapoints(dplist)
-                if(len(currentDataPoints) >= 3):
-                    location_tag =  tri.trilateration(currentDataPoints)
-                    tag.update_xy(location_tag.x,location_tag.y,location_tag.z,time.time())
-                cc1, addr1 = socket_for_sending.accept()
-                cc1.send(bytes(str([time.time(), data2[3],data2[4],data2[5],data2[6],data2[7],data2[8],data2[9],data2[10],data2[11], str(tag.x), str(tag.y), tag.user]).encode()))
-                #cc1.close()
+
+def filesave(data_save):
+    inp1 = open("C:\\Users\\smchakra\\Desktop\\Data.txt","a+")
+    #print(data_save[0])
+    inp1.write(str(data_save))
+    inp1.write("\n")
+    inp1.close()
+
+dplist = {}
+f = open('C:\\Users\\smchakra\\Desktop\\Experiments\\Experiments_Python\\Construction site\\Experiment_4\\Raw\\walking_1_head.txt','r')
+line = f.readline()
+splitted = line.split(",")
+timestamp = float(splitted[0].strip("[").strip("'"))
+time_window = 1000.0 # time window to consider the anchor readings in
+moving_window = 400.0 # moving window
+location_tag = np.array([0.0,0.0],float).reshape(-1,1)
+try:
+    while line:
+        line = f.readline()
+        line = line.replace("'", '')
+        splitted = line.rstrip().split(",")
+        data = splitted
+        if(len(splitted)==15 and float(splitted[2].strip("' '"))>0 and float(splitted[14].replace("'", '').strip().strip("]"))>-105): # RSSI greater than -105 and the distance should not be negative
+            x = datapoint(float(splitted[0].rstrip().strip("[").strip("'")),(str(splitted[3].strip().replace("'", ''))), float(splitted[14].replace("'", '').strip().strip("]")),float(splitted[2].strip("' '")))
+            values = x.get_dp()
+            if(float(splitted[0].rstrip().strip("[").strip("'")) - timestamp < time_window):
+                dplist[str(splitted[3].strip())] = values
             else:
-                pass
-        finally:
-            pass
+                if(len(dplist)>=3):
+                    location_tag_current = tri.trilateration(dplist)
+                    data.append(str(location_tag_current[0]))
+                    data.append(str(location_tag_current[1]))
+                    location_tag = np.hstack((location_tag, location_tag_current.reshape(-1,1)))
+                timestamp = float(splitted[0].rstrip().strip("[").strip("'"))
+                currentDataPoints = rcd.resetcurrentdatapoints(timestamp, dplist)
+        #filesave(data)
+finally:
+    pass
+    plt.scatter(location_tag[0, :].flatten(), location_tag[1, :].flatten())
+    plt.plot(location_tag[0, :].flatten(), location_tag[1, :].flatten(), "-k")
+    plt.axis("equal")
+    plt.grid(True)
